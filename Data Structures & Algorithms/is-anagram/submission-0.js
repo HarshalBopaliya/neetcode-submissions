8c7ab0d61e0ff069223a8b41f620202a3b07a1aa
HarshalBopaliya <132598460+HarshalBopaliya@users.noreply.github.com>
2026-07-28 19:27:53 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+         // If lengths are different, they can't be anagrams
+        if (s.length !== t.length) {
+            return false;
+        }
+        const count = {};
+
+      // Count characters in s
+      for(const char of s){
+        count[char] = (count[char] || 0) +1 ;
+      }
+
+      // Decrease counts using t
+      for (const char of t){
+        if(!count[char]){
+            return false;
+        }
+        count[char]--;
+      }
+
+      return true;
+
+    }
+}
